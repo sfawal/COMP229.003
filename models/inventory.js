2051d6598c4b67607243e319721e4db983747f87
sfawal <sfawal@my.centennialcollege.ca>
2022-03-12 13:00:46 -0500

    Assignment #2 4.0

--- models/inventory.js
+++ models/inventory.js
@@ -8,14 +8,17 @@ let mongoose = require('mongoose');
 //collection of items that will be recorded in business list
 let inventoryModel = mongoose.Schema(
     {
-        item:String,
+        item: String,
         qty: Number,
-        status: String,
+        
+        status: {
+            type:String,
+            match: [/.+\@.+\..+/, "Please enter a valid e-mail address"]
+        }
     },
     {
         collection: "inventory"
     }
-
 );
 
 module.exports = mongoose.model('Inventory', inventoryModel);
